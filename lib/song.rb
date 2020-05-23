143d20c2b65e0aa96cd7ca96907721185e159d21
Ben Parish <parishben13@gmail.com>
2020-05-23 18:40:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 class Song 
   attr_accessor :name, :artist, :genre
-  
+  @@count = 0
     def artists 
   end
   def artist_count
   end
   def genre_count
 end
-@@count = 0
+def self.count
+  @@count
 end
