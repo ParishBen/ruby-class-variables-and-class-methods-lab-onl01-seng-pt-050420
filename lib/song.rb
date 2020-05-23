202f664f0971755a0bd564d576f92489c8eff72c
Ben Parish <parishben13@gmail.com>
2020-05-23 20:51:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,14 @@ def self.count
   end
   
  def self.genre_count
-   genresarr=[]
-   genrearr2=[]
-  
-   
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
     end
 end
